aa346644f3090bc9d442a5610dc93ef3c806a145
Augusto Castilho <ac.castilho87@gmail.com>
2025-02-13 00:10:02 -0300

    Estudos Python

--- 17-funcoes_pt2.py
+++ 17-funcoes_pt2.py
@@ -17,10 +17,18 @@ print("____________________________________")
 
 salario = 2000
 
-def salario_bonus(bonus):
+def salario_bonus(bonus, lista):
     global salario
+    
+    lista_aux = lista.copy()
+    lista_aux.append(2)
+    print(f"lista aux={lista_aux}")
+    
     salario += bonus
     return salario
 
-salario_com_bonus = salario_bonus(500)
-print(f"O novo salário é: {salario_com_bonus}")
+
+lista = [1]
+salario_com_bonus = salario_bonus(500, lista)
+print(salario_com_bonus)
+print(lista)
